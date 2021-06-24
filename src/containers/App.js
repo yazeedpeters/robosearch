ab26ed96f81db96c2e94b30e9ebd5718f4cd09c1
gnostic_knowmad <69956514+yazeedpeters@users.noreply.github.com>
2021-06-24 16:53:07 +0200

    bulk upload

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/SearchBox.js';
+import Scroll from '../components/scroll.js'; 
+import ErrorBoundary from '../components/ErrorBoundary.js';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+    
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        return !robots.length ? 
+        <h1>Loading...</h1> :
+        (
+            <div className='tc'>
+                <h1 className='f1' >RoboSearch</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <ErrorBoundary>
+                    <CardList robots={filteredRobots} />
+                </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
+    }
+}
+
+export default App;
